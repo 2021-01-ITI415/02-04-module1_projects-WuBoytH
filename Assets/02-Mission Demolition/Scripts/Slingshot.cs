32b8c72a47add2bc7b88238939484e3da781e5d3
WuBoytH <alexwuief@gmail.com>
2021-02-11 11:43:22 -0500

    Wrote a script to see if launchpoint is active

---
+++ Assets/02-Mission Demolition/Scripts/Slingshot.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slingshot : MonoBehaviour
+{
+    public GameObject launchPoint;
+    void OnMouseEnter() {
+        print("Slingshot:OnMouseEnter()");
+        launchPoint.SetActive(true);
+    }
+    void OnMouseExit() {
+        print("Slingshot:OnMouseExis()");
+        launchPoint.SetActive(false);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    void Awake(){
+        Transform launchPointTrans = transform.Find("LaunchPoint");
+        launchPoint = launchPointTrans.gameObject;
+        launchPoint.SetActive(false);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
