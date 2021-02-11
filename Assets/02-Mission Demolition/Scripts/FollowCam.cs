9ed12142ac00dcc10c38d366698dd2fd63db05e5
WuBoytH <alexwuief@gmail.com>
2021-02-11 12:47:57 -0500

    Made Clouds and a Castle

--- Assets/02-Mission Demolition/Scripts/FollowCam.cs
+++ Assets/02-Mission Demolition/Scripts/FollowCam.cs
@@ -6,6 +6,10 @@ public class FollowCam : MonoBehaviour
 {
     static public GameObject POI;
 
+    [Header("Set in Inspector")]
+    public float easing = 0.05f;
+    public Vector2 minXY = Vector2.zero;
+
     [Header("Set Danymically")]
     public float camZ;
 
@@ -23,8 +27,12 @@ public class FollowCam : MonoBehaviour
         if(POI == null) return;
 
         Vector3 destination = POI.transform.position;
+        destination.x = Mathf.Max(minXY.x, destination.x);
+        destination.y = Mathf.Max(minXY.y, destination.y);
+        destination = Vector3.Lerp(transform.position, destination, easing);
         destination.z = camZ;
         transform.position = destination;
+        Camera.main.orthographicSize = destination.y + 10;
     }
     // Update is called once per frame
     void Update()
