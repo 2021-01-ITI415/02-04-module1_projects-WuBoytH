8dc70592883076580d94010e8800794f0710fb90
WuBoytH <alexwuief@gmail.com>
2021-02-11 13:52:43 -0500

    Built more castles and made UI

--- Assets/02-Mission Demolition/Scripts/Slingshot.cs
+++ Assets/02-Mission Demolition/Scripts/Slingshot.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Slingshot : MonoBehaviour
 {
+    static private Slingshot S;
+
     [Header("Set in Inspector")]
     public GameObject prefabProjectile;
     public float velocityMult = 8f;
@@ -14,6 +16,13 @@ public class Slingshot : MonoBehaviour
     public GameObject projectile;
     public bool aimingMode;
     private Rigidbody projectileRigidbody;
+
+    static public Vector3 LAUNCH_POS {
+        get {
+            if(S == null) return Vector3.zero;
+            return S.launchPos;
+        }
+    }
 
     void OnMouseEnter() {
         print("Slingshot:OnMouseEnter()");
@@ -29,6 +38,7 @@ public class Slingshot : MonoBehaviour
         
     }
     void Awake(){
+        S = this;
         Transform launchPointTrans = transform.Find("LaunchPoint");
         launchPoint = launchPointTrans.gameObject;
         launchPoint.SetActive(false);
