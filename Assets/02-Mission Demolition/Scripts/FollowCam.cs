21d80980523fadd4ba4eb213fddec66ab49b6d5c
WuBoytH <alexwuief@gmail.com>
2021-02-11 12:18:53 -0500

    Added projectile physics and camera movement

---
+++ Assets/02-Mission Demolition/Scripts/FollowCam.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour
+{
+    static public GameObject POI;
+
+    [Header("Set Danymically")]
+    public float camZ;
+
+    // Start is called before the first frame update
+    void Awake(){
+        camZ = this.transform.position.z;
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate(){
+        if(POI == null) return;
+
+        Vector3 destination = POI.transform.position;
+        destination.z = camZ;
+        transform.position = destination;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
